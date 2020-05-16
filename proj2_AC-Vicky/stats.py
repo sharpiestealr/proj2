b378000130a3fa3f9027b2746a455c71243d6983
acsouza2398 <anaclgs@al.insper.edu.br>
2020-05-15 21:04:21 -0300

    Added player stats and level up mechanic

--- proj2_AC-Vicky/stats.py
+++ proj2_AC-Vicky/stats.py
@@ -1,4 +1,27 @@
-class player(object):
+class Player(object):
     """a place to keep global vars + control vars"""
+        #player stats
+    def name():
+        self.name = input("What's your name, adventurer? ")  
+    self.hp = 10+2*Player.level
+    self.atk = 3+1*Player.level
+    self.df = 3+1*Player.level
+    self.xp = 0
+    self.threshold = 10
+    self.level = 1
+    self.chapter = 1
+      
+    #combat variables
+    self.dmg = 0
+    self.res = 0
+    self.attack = " "
 
+#player choice, going to be buttons in final version. Using input as a preliminary version
+Player.attack = input("attack, defense or magic")
 
+#check lvl up post combat
+def lvl_up(Player.xp, Player.threshold, Player.level):
+    if Player.xp >= Player.threshold:
+        Player.xp = Player.xp - Player.threshold
+        Player.level = Player.level + 1
+        Player.threshold = Player.threshold*2
