e0a26ac1be3d3d371314d797f9f7efbd33fc1d04
sharpiestealr <62366339+sharpiestealr@users.noreply.github.com>
2020-06-15 18:41:48 -0300

    Revert "got rid of useless stuff"
    This reverts commit 5d0a94fc313858c86e0fc8a5314b4564f6dc441c.

---
+++ proj2_AC-Vicky/combat_LOCAL_1716.py
@@ -0,0 +1,152 @@
+import pygame as pg
+import os
+import stats
+import enemies
+
+#determining player and enemy stats
+plat = stats.Player()
+enem = enemies.Enemy('goblin', 'no')
+enem_gen = random.randint(0,3)
+boss = 'no'
+    if plat.room == "final":
+        boss = 'yes'
+if enem_gen == 1:
+    enem = enemies.Enemy('goblin', boss)
+    enem_sprite = "goblin mockup.png"
+    enem_name = "goblin"
+elif enem_gen == 2:
+    enem = enemies.Enemy('gnome', boss)
+    enem_sprite = "gnome mockup.png"
+    enem_name = "gnome"
+else:
+    enem = enemies.Enemy('slime', boss)
+    enem_sprite = "slime mockup.png"
+    enem_name = "slime"
+
+result = 0
+
+#importing sprite folders
+current_path = os.path.dirname(__file__)
+@@ -13,16 +29,15 @@
+pg.init()
+
+#creating screen and background
+screen = pg.display.set_mode((640,480))
+background = pg.Surface(screen.get_size())
+background.fill((17,143,139))
+screen = pg.display.set_mode((1280,720))
+background = pg.image.load(os.path.join(image_path, "combat bg.jpg"))
+background = background.convert()
+
+screen.blit(background, (0,0))
+pg.display.update()
+
+#creating box for player to pick action
+box = pg.draw.rect(screen, [249, 228, 183], [0, 300, 640, 213], 0)
+box = pg.draw.rect(screen, [249, 228, 183], [0, 500, 1280, 213], 0)
+pg.display.update()
+
+font = pg.font.Font('freesansbold.ttf', 24)
+@@ -40,12 +55,12 @@
+textpRect = textp.get_rect()
+texteRect = texte.get_rect()
+
+text1Rect.center = (540, 340)
+text2Rect.center = (540, 390)
+text3Rect.center = (540, 440)
+textcRect.center = (320, 390)
+textpRect.center = (100, 340)
+texteRect.center = (100, 390)
+text1Rect.center = (1100, 540)
+text2Rect.center = (1100, 590)
+text3Rect.center = (1100, 640)
+textcRect.center = (880, 590)
+textpRect.center = (100, 540)
+texteRect.center = (100, 590)
+
+screen.blit(text1, text1Rect)
+screen.blit(text2, text2Rect)
+@@ -56,43 +71,75 @@
+pg.display.update()
+
+#creating cursor for choice
+ypos = 340
+ypos = 540
+posi = 1
+choice = pg.draw.circle(screen, [0, 0, 0], [470, ypos], 9)
+choice = pg.draw.circle(screen, [0, 0, 0], [1030, ypos], 9)
+
+pg.display.update()
+
+#placing character and enemy sprites
+player_char = pg.image.load(os.path.join(image_path, "player.png"))
+enem_char = pg.image.load(os.path.join(image_path, enem_sprite))
+
+screen.blit(player_char, (1030,200))
+screen.blit(enem_char, (250,200))
+pg.display.update()
+
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT: 
+            running = False
+        elif event.type == pg.KEYDOWN:
+            if result != 0:
+                running = False
+                break
+            if event.key == pg.K_ESCAPE:
+                running = False
+                break
+            elif event.key == pg.K_DOWN:
+                if posi < 3:
+                    ypos = ypos + 50
+                    posi = posi + 1
+                    print(posi)
+            elif event.key == pg.K_UP:
+                if posi > 1:
+                    ypos = ypos - 50
+                    posi = posi - 1
+            elif event.key == pg.K_RETURN:
+                h = enemies.combat("goblin", "no", posi)
+                healthp = h[0]
+                healthe = h[1]
+                h = enemies.combat(enem_name, "no", posi)
+                if h[0] <= 0:
+                    healthp = 0
+                    result = 2
+                else:
+                    healthp = h[0]
+
+                if h[1] <= 0:
+                    healthe = 0
+                    result = 1
+                else:
+                    healthe = h[1]
+
+                textp = font.render("Player: {0}".format(healthp), True, [0, 0, 0])
+                texte = font.render("{0}: {1}".format(enem.mob, healthe), True, [0, 0, 0])
+    box = pg.draw.rect(screen, [249, 228, 183], [0, 300, 640, 213], 0)
+
+    if result == 1:
+        result_text = font.render('Victory!', True, [0, 0, 0],[249,228, 183])
+        resultRect = result_text.get_rect()
+        resultRect.center = (640, 360)
+        screen.blit(result_text, resultRect)
+    elif result == 2:
+        result_text = font.render('Lost!', True, [0, 0, 0],[249,228, 183])
+        resultRect = result_text.get_rect()
+        resultRect.center = (640, 360)
+        screen.blit(result_text, resultRect)     
+
+    box = pg.draw.rect(screen, [249, 228, 183], [0, 500, 1280, 213], 0)
+    screen.blit(text1, text1Rect)
+    screen.blit(text2, text2Rect)
+    screen.blit(text3, text3Rect)
+    screen.blit(textc, textcRect)
+    screen.blit(textp, textpRect)
+    screen.blit(texte, texteRect)
+    choice = pg.draw.circle(screen, [0, 0, 0], [470, ypos], 9)
+    choice = pg.draw.circle(screen, [0, 0, 0], [1030, ypos], 9)
+    pg.display.update()
