e7132b91d312e7b5f8395f90abfed6e56540cc72
acsouza2398 <anaclgs@al.insper.edu.br>
2020-06-15 20:51:22 -0300

    Fixed combat once and for all. Moved everything to do with combat over and deleted enemies because it was no longer needed

--- proj2_AC-Vicky/stats.py
+++ proj2_AC-Vicky/stats.py
@@ -1,8 +1,6 @@
 class Player:
     """a place to keep player info"""
     #player stats
-    level = 1
-    xp = 0
     croom = "opening" #used to control the boss factor
     lastroom = ""
     coins = 0
@@ -23,36 +21,12 @@ class Player:
     isFall = False
     jumpCount = 0
     locat = 0
+    hp = 12
+    atk = 4
+    df = 4
+    dmg = 0
+    res = 0
     player = [x, y, step_x, step_y, isJump, isFall, jumpCount, locat]
     player_last = [x, y, step_x, step_y, isJump, isFall, jumpCount, locat]
-    stop = 0
-
-    def __init__(self):
-        #self.name = input("What's your name, adventurer? ")  
-        self.tothp = 10+2*self.level
-        self.atk = 3+1*self.level
-        self.df = 3+1*self.level
-        self.threshold = 10*self.level
-
-        #combat variables
-        self.dmg = 0
-        self.res = 0
-        self.result = 0
-        self.hp = self.tothp
-        print(self.hp)        
-    
-    def pattack(self, posi):
-        if posi == 1:
-            self.action = "attack"
-        elif posi == 2:
-            self.action = "defense"
-        else:
-            self.action = "magic"
-
-    #check lvl up post combat
-    def lvl_up(self):
-        if self.xp >= self.threshold:
-            self.xp = self.xp - self.threshold
-            self.level = self.level + 1
-            self.threshold = self.threshold*2
-            print("Congratulations! You leveled up!")
+    player_combat = [hp, atk, df, dmg, res]
+    stop = 0  
