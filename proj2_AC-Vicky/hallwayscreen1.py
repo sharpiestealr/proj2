798541ecfbbb10aeac78d9038c28022e42ba621f
sharpiestealr <62366339+sharpiestealr@users.noreply.github.com>
2020-06-16 01:14:54 -0300

    three first screens now have flips
    holy flips, batman!

--- proj2_AC-Vicky/hallwayscreen1.py
+++ proj2_AC-Vicky/hallwayscreen1.py
@@ -15,7 +15,12 @@ class Player_s(pygame.sprite.Sprite):
         self.isFall = False
         self.jumpCount = 11
         self.locat = 1 # 1 = ground
+        self.left = False
+        self.haveflip = 1
     def update(self):
+        if self.haveflip == 0:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.haveflip = 1
         self.rect.x = self.x
         self.rect.y = self.y
 
@@ -175,11 +180,19 @@ def hallway_run(plat, running):
                         player.isFall = False
 
         if keys[pygame.K_RIGHT] and player.x < 1150:
-            player.x = player.x + player.step_x
-       
+            if plat.player.left == True:
+                plat.player.left = False
+                plat.player.haveflip = 0
+                plat.player.x = plat.player.x + plat.player.step_x
+            else:
+                plat.player.x = plat.player.x + plat.player.step_x       
         if keys[pygame.K_LEFT] and player.x > 50:
-           player.x = player.x - player.step_x
-
+            if plat.player.left == False:
+                plat.player.left = True
+                plat.player.haveflip = 0
+                plat.player.x = plat.player.x - plat.player.step_x
+            else:
+                plat.player.x = plat.player.x - plat.player.step_x
         if player.locat == 1:
             if player.isJump:
                 player.rect.y = player.image.get_height() + player.step_y
